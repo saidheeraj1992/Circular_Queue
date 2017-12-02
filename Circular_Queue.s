49822e7414c0406b41bf99dd28284f2c63b57b25
saidheeraj1992 <33399930+saidheeraj1992@users.noreply.github.com>
2017-12-03 02:37:14 +0530

    Initial commit

---
+++ Circular_Queue.s
@@ -0,0 +1,55 @@
+     PRESERVE8
+     THUMB
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+__main  FUNCTION		 
+        MOV r5, #0x20000000 ; tail address
+		MOV r3, #0x20000000 ; head address
+		MOV r1, #0x11;       initial value
+		MOV r2, #0xA;       queue size
+		
+loop 	CMP r2,#0;
+        STR r1,[r5];
+        ADD r5,#0x4;
+		EOR r1,r1,r2;
+		SUBGT r2,r2,#1;
+		BGT loop;
+		
+		MOV r2,#0x4;
+loop1	CMP r2,#0;
+        MOV r1,#0;		
+        STR r1,[r3];
+        ADD r3,#0x4;
+		SUBGT r2,r2,#1;
+		BGT loop1;
+			  
+		MOV r5, #0x20000000;
+		MOV r2,#0x4;
+loop2 	CMP r2,#0;
+		EOR r1,r1,r2;
+        STR r1,[r5];
+        ADD r5,#0x4; 
+		SUBGT r2,r2,#1;
+		BGT loop2;
+			  
+		MOV r2,#0x6;
+loop3	CMP r2,#0;
+        MOV r1,#0;		
+        STR r1,[r3];
+        ADD r3,#0x4;
+		SUBGT r2,r2,#1;
+		BGT loop3;
+			  
+		MOV r3, #0x20000000;
+		MOV r2,#0x4;
+loop4	CMP r2,#0;
+        MOV r1,#0;		
+        STR r1,[r4];
+        ADD r3,#0x4;
+		SUBGT r2,r2,#1;
+		BGT loop4;				
+       
+stop B stop ; stop program
+     ENDFUNC
+     END
