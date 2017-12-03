7112e0cb3e7b01d9615edb8ab744e102f879f8af
saidheeraj1992 <33399930+saidheeraj1992@users.noreply.github.com>
2017-12-03 08:15:02 +0530

    Update Circular_Queue.s

--- Circular_Queue.s
+++ Circular_Queue.s
@@ -9,15 +9,15 @@ __main  FUNCTION
 		MOV r1, #0x11;       initial value
 		MOV r2, #0xA;       queue size
 		
-loop 	CMP r2,#0; Push data
+loop 	CMP r2,#0; Push data (All 10 elements) 
         STR r1,[r5];
         ADD r5,#0x4;
-		EOR r1,r1,r2;
+		EOR r1,r1,r2; Randomness added
 		SUBGT r2,r2,#1;
 		BGT loop;
 		
 		MOV r2,#0x4;
-loop1	CMP r2,#0; POP some data
+loop1	CMP r2,#0; POP some data (4 elements)
         MOV r1,#0;		
         STR r1,[r3];
         ADD r3,#0x4;
